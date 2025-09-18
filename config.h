acfcf925504687061b3ccb75a0b0120828ce7e34
maturuuu <matthew_kristoffer_ong@dlsu.edu.ph>
2025-09-18 17:51:02 +0800

    feat: divide by search range (print afterwards) done

--- config.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-const int threadX = 3; //number of threads to spawn
-const int numY = 1000; //number up to which prime numbers will be searched for
--- main.cpp
+++ main.cpp
@@ -4,15 +4,15 @@
 #include <iomanip>
 #include <math.h>
 #include <mutex>
-#include "config.h"
 
 using namespace std;
 mutex printMutex;
 
 vector<thread> threads; //global thread container
+vector<string> outputBuffer; //output buffer for printing afterwards
 chrono::steady_clock::time_point startTime; //holds the start time, updated per run
 
-string getTimestamp(){
+string getTimestamp(){ //time from startTime
     auto now = chrono::steady_clock::now();
     auto diff = now - startTime;
     ostringstream timeString;
@@ -30,7 +30,7 @@ string getTimestamp(){
     return timeString.str();
 }
 
-string getCurrentTime(){
+string getCurrentTime(){ //system time right now
     auto now = chrono::system_clock::now();
     auto millis = duration_cast<chrono::milliseconds>(now.time_since_epoch()).count() % 1000;
     ostringstream timeNow;
@@ -45,11 +45,23 @@ string getCurrentTime(){
     return timeNow.str();
 }
 
-void joinAndClearThreads(){
-    for(thread &thread : threads){
-        thread.join();
+void printBuffer(){
+    for(auto &log : outputBuffer){
+        cout << log << "\n";
     }
+}
+
+void joinThreads(){
+    for(thread &thread : threads){
+        if (thread.joinable()) {
+            thread.join();
+        }
+    }
+}
+
+void cleanup(){
     threads.clear();
+    outputBuffer.clear();
 }
 
 bool checkIfPrime(int num){
@@ -78,6 +90,25 @@ void checkIfPrimeRange(int start, int end, int id){
     }
 }
 
+void checkIfPrimeRangeBuffer(int start, int end, int id){
+    for(int num=start; num<end; num++){
+        if(checkIfPrime(num) == true){
+            string timestamp = getTimestamp();
+            ostringstream logString;
+
+            logString << timestamp << " Thread " << id << " | " << num << " -> PRIME" << "\n";
+            outputBuffer[num] = logString.str();
+        }
+        else {
+            string timestamp = getTimestamp();
+            ostringstream logString;
+
+            logString << timestamp << " Thread " << id << " | " << num << " -> NOT PRIME" << "\n";
+            outputBuffer[num] = logString.str();
+        }
+    }
+}
+
 //Task division scheme: straight division of search range (print immediately) DONE!!
 void threadByRange1(int threadCount, int maxNum){
     int adjustedMaxNum = maxNum + 1;
@@ -92,7 +123,17 @@ void threadByRange1(int threadCount, int maxNum){
 }
 
 //Task division scheme: straight division of search range (print afterwards) TODO!!
-void threadByRange2(){}
+void threadByRange2(int threadCount, int maxNum){
+    int adjustedMaxNum = maxNum + 1;
+
+    int rangeSize = (adjustedMaxNum + threadCount - 1) / threadCount;
+
+    for(int i=0; i<threadCount; i++){
+        int start = i * rangeSize;
+        int end = min(start + rangeSize, adjustedMaxNum);
+        threads.emplace_back(checkIfPrimeRangeBuffer, start, end, i);
+    }
+}
 
 //Task division scheme: threads are for divisibility testing (print immediately)
 void threadByDiv1(){}
@@ -103,17 +144,105 @@ void threadByDiv2(){}
 
 int main(){
     string algoStartTime;
+    int threadX, numY;
 
-    //algorithm start (put into selector later)
-    cout << "Checking for primes up to number " << numY << " using " << threadX << " threads." << "\n";
-    startTime = chrono::steady_clock::now();
-    algoStartTime = getCurrentTime();
-    cout << "Start time | " << algoStartTime << "\n";
+    // menu stuff
+    bool exit = false;
+    int choice;
 
-    threadByRange1(threadX, numY);
-    joinAndClearThreads();
-    
-    cout << "Start time | " << algoStartTime << "\n";
-    cout << "End time   | " << getCurrentTime() << "\n";
-    cout << "Total time elapsed -> " << getTimestamp() << "\n";
+    while(true) {
+            cout << "Enter the number of threads: ";
+            if(cin >> threadX && threadX > 0) {
+                break;
+            } else {
+                cout << "Invalid input. Please enter a positive integer.\n";
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            }
+        }
+
+    while(true) {
+        cout << "Enter the maximum number to check: ";
+        if(cin >> numY && numY > 0) {
+            break;
+        } else {
+            cout << "Invalid input. Please enter a positive integer.\n";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+    }
+
+    while(!exit){
+        cout << "\n=============== MENU ===============\n";
+        cout << "Threads: " << threadX << " | " << "Max number: " << numY << "\n";
+        cout << "1. Divide by search range (print immediately)\n";
+        cout << "2. Divide by search range (print afterwards)\n";
+        cout << "3. Divide by divisibility (print immediately)\n";
+        cout << "4. Divide by divisibility (print afterwards)\n";
+        cout << "5. Exit\n";
+        cout << "Select an option (1-5): ";
+
+        if (!(cin >> choice)) {
+        cout << "Invalid input! Please enter a number.\n";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        continue;
+        }
+
+        switch(choice) {
+            case 1:
+                cout << "Checking for primes up to number " << numY << " using " << threadX << " threads." << "\n";
+                startTime = chrono::steady_clock::now();
+                algoStartTime = getCurrentTime();
+                cout << "Start time | " << algoStartTime << "\n";
+
+                threadByRange1(threadX, numY);
+                joinThreads();
+                
+                cout << "Start time | " << algoStartTime << "\n";
+                cout << "End time   | " << getCurrentTime() << "\n";
+                cout << "Total time elapsed -> " << getTimestamp() << "\n";
+
+                cleanup();
+                break;
+
+            case 2:
+                cout << "Checking for primes up to number " << numY << " using " << threadX << " threads." << "\n";
+                startTime = chrono::steady_clock::now();
+                algoStartTime = getCurrentTime();
+                cout << "Start time | " << algoStartTime << "\n";
+
+                outputBuffer.resize(numY+1);
+                threadByRange2(threadX, numY);
+                joinThreads();
+                printBuffer();
+                
+                cout << "Start time | " << algoStartTime << "\n";
+                cout << "End time   | " << getCurrentTime() << "\n";
+                cout << "Total time elapsed -> " << getTimestamp() << "\n";
+
+                cleanup();
+                break;
+
+            case 3:
+                cout << "Checking for primes up to number " << numY << " using " << threadX << " threads." << "\n";
+                startTime = chrono::steady_clock::now();
+                algoStartTime = getCurrentTime();
+                cout << "Start time | " << algoStartTime << "\n";
+
+                break;
+
+            case 4:
+                cout << "Checking for primes up to number " << numY << " using " << threadX << " threads." << "\n";
+                startTime = chrono::steady_clock::now();
+                algoStartTime = getCurrentTime();
+                cout << "Start time | " << algoStartTime << "\n";
+
+                break;
+                
+            case 5:
+                exit = true;
+                break;
+        }
+    }
 }
